8ad85cbb0f0ef0b53f1b3ef40cb54d0de7c39697
David Brown <davewbrwn@gmail.com>
2020-02-11 23:40:07 -0700

    A tracking of validation loss

--- train.py
+++ train.py
@@ -5,21 +5,23 @@ from models.posenet import PoseNet
 import os
 from torch.utils.tensorboard import SummaryWriter
 from datetime import datetime
+import numpy as np
 
 # default `log_dir` is "runs" - we'll be more specific here
 timestr = datetime.now().strftime("%m-%d-Y-%H-%M-%S")
-writer = SummaryWriter(f'runs/posenet-{timestr}')
+writer = SummaryWriter(f'runs/posenet')
 
 dataset = UnityEyesDataset()
 N = len(dataset)
 
-TN = int(N * 0.8)
-VN = N - TN
+VN = 100
+TN = N - VN
 train_set, val_set = torch.utils.data.random_split(dataset, (TN, VN))
 
 dataloader = DataLoader(train_set, batch_size=4,
                         shuffle=True, num_workers=8)
-
+valDataLoader = DataLoader(val_set, batch_size=4,
+                        shuffle=True, num_workers=8)
 
 posenet = PoseNet(nstack=8, inp_dim=256, oup_dim=32)
 
@@ -37,7 +39,7 @@ if os.path.exists('checkpoint'):
     optimizer.load_state_dict(checkpoint['optimizer_state_dict'])
 
 for i_batch, sample_batched in enumerate(dataloader):
-    if i_batch % 2 == 0:
+    if i_batch % 20 == 0:
         torch.save({
             'model_state_dict': posenet.state_dict(),
             'optimizer_state_dict': optimizer.state_dict(),
@@ -51,10 +53,24 @@ for i_batch, sample_batched in enumerate(dataloader):
     Yp = posenet.forward(X)
 
     loss = torch.mean(posenet.calc_loss(Yp, Y))
-
-    print(i_batch, loss.item())
-    writer.add_scalar("Training Loss", 1000 * loss.item(), i_batch)
-
     optimizer.zero_grad()
     loss.backward()
     optimizer.step()
+
+    print(i_batch, 'training loss', loss.item())
+    writer.add_scalar("training Loss", 1000 * loss.item(), i_batch)
+
+    if i_batch % 10 == 0:
+        with torch.no_grad():
+            val_losses = []
+            for val_batch in valDataLoader:
+                X = torch.tensor(val_batch['img'], dtype=torch.float32)
+                Y = val_batch['heatmaps']
+                Yp = posenet.forward(X)
+                loss = torch.mean(posenet.calc_loss(Yp, Y))
+                val_losses.append(loss.item())
+            val_loss = np.mean(val_losses)
+            writer.add_scalar("talidation Loss", 1000 * val_loss, i_batch)
+        print(i_batch, 'validation loss', val_loss)
+
+
